df985f83b979f8b40e259e2e04759f1a04c4d58c
Ahthesham Ali <ahtheshamali786@gmail.com>
2024-09-13 21:29:52 -0400

    fixed some bugs :((((

--- src/components/chatbot/window.tsx
+++ src/components/chatbot/window.tsx
@@ -84,7 +84,7 @@ export const BotWindow = forwardRef<HTMLDivElement, Props>(
             </Avatar>
             <div className="flex items-start flex-col">
               <h3 className="text-lg font-bold leading-none">
-                Customer Rep - Amplifier AI
+                Sales Rep - Web Prodigies
               </h3>
               <p className="text-sm">{domainName.split('.com')[0]}</p>
               {realtimeMode?.mode && (
@@ -178,7 +178,7 @@ export const BotWindow = forwardRef<HTMLDivElement, Props>(
           </TabsContent>
         </TabsMenu>
         <div className="flex justify-center ">
-          <p className="text-gray-400 text-xs">Powered By Amplifier AI</p>
+          <p className="text-gray-400 text-xs">Powered By Web Prodigies</p>
         </div>
       </div>
     )

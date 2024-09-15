5a1b04f163499d7cffa7f3bd6aa561d8b6db5511
Ahthesham Ali <ahtheshamali786@gmail.com>
2024-09-15 16:15:31 -0400

    alhamdulillah fixed the date error

--- src/constants/timeslots.ts
+++ src/constants/timeslots.ts
@@ -2,55 +2,56 @@ type AppointmentTimeSlots = {
   slot: string
 }
 
+//WIP: Need to fix the styling in the potel it goes outside the page when more time slots are added
 export const APPOINTMENT_TIME_SLOTS: AppointmentTimeSlots[] = [
-  { 
-    slot: '8:00am' 
-  },
-  { 
-    slot: '8:30am' 
-  },
-  { 
-    slot: '9:00am' 
-  },
-  { 
-    slot: '9:30am' 
-  },
-  { 
-    slot: '10:00am' 
-  },
-  { 
-    slot: '10:30am' 
-  },
-  { 
-    slot: '11:00am' 
-  },
-  { 
-    slot: '11:30am' 
-  },
-  { 
-    slot: '12:00pm' 
-  },
-  { 
-    slot: '12:30pm' 
-  },
-  { 
-    slot: '1:00pm' 
-  },
-  { 
-    slot: '1:30pm' 
-  },
-  { 
-    slot: '2:00pm' 
-  },
-  { 
-    slot: '2:30pm' 
-  },
-  { 
-    slot: '3:00pm' 
-  },
-  { 
-    slot: '3:30pm' 
-  },
+  // { 
+  //   slot: '8:00am' 
+  // },
+  // { 
+  //   slot: '8:30am' 
+  // },
+  // { 
+  //   slot: '9:00am' 
+  // },
+  // { 
+  //   slot: '9:30am' 
+  // },
+  // { 
+  //   slot: '10:00am' 
+  // },
+  // { 
+  //   slot: '10:30am' 
+  // },
+  // { 
+  //   slot: '11:00am' 
+  // },
+  // { 
+  //   slot: '11:30am' 
+  // },
+  // { 
+  //   slot: '12:00pm' 
+  // },
+  // { 
+  //   slot: '12:30pm' 
+  // },
+  // { 
+  //   slot: '1:00pm' 
+  // },
+  // { 
+  //   slot: '1:30pm' 
+  // },
+  // { 
+  //   slot: '2:00pm' 
+  // },
+  // { 
+  //   slot: '2:30pm' 
+  // },
+  // { 
+  //   slot: '3:00pm' 
+  // },
+  // { 
+  //   slot: '3:30pm' 
+  // },
   { 
     slot: '4:00pm' 
   },

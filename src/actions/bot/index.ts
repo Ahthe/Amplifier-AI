336361d2f79a938adf5cee38a5ca4a24241bb226
Ahthesham Ali <ahtheshamali786@gmail.com>
2024-09-15 16:40:59 -0400

    alhamdulillah the ai bot is fixed

--- src/actions/bot/index.ts
+++ src/actions/bot/index.ts
@@ -241,6 +241,9 @@ export const onAiChatBotAssistant = async (
               if the customer wants to buy a product redirect them to the payment page https://amplifire-ai.vercel.app/portal/${id}/payment/${
                 checkCustomer?.customer[0].id
               }
+
+              Always maintain a respectful and professional tone.
+              Array of questions: [${chatBotDomain.filterQuestions.map((questions) => questions.question).join(', ')}]
           `,
             },
             ...chat,
@@ -249,7 +252,7 @@ export const onAiChatBotAssistant = async (
               content: message,
             },
           ],
-          model: 'gpt-3.5-turbo',
+          model: 'gpt-4o-mini',
         })
 
         if (chatCompletion.choices[0].message.content?.includes('(realtime)')) {
